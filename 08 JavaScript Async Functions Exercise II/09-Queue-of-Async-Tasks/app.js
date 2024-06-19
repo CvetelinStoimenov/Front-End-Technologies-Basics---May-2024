8ac644528dcd05b516adbabd4bf4dfa2e6476a31
CvetelinStoimenov <stoimenovv@gmail.com>
2024-06-19 14:30:20 +0300

    19.06.2024

--- 08 JavaScript Async Functions Exercise II/09-Queue-of-Async-Tasks/app.js
+++ 08 JavaScript Async Functions Exercise II/09-Queue-of-Async-Tasks/app.js
@@ -1,7 +1,27 @@
 class AsyncQueue {
-    // TODO...
+    constructor() {
+        this.queue = [];
+        this.processing = false;
+    }
+
+    enqueue(task) {
+        this.queue.push(task);
+        if (!this.processing) this.process();
+    }
+
+    async process() {
+        this.processing = true;
+        while (this.queue.length > 0) {
+            const task = this.queue.shift();
+            await task();
+        }
+        this.processing = false;
+    }
 }
 
 function startQueue() {
-    // TODO...
+    const queue = new AsyncQueue();
+    queue.enqueue(() => new Promise(resolve => setTimeout(() => { console.log("Task 1 done"); resolve(); }, 1000)));
+    queue.enqueue(() => new Promise(resolve => setTimeout(() => { console.log("Task 2 done"); resolve(); }, 1500)));
+    queue.enqueue(() => new Promise(resolve => setTimeout(() => { console.log("Task 3 done"); resolve(); }, 500)));
 }
