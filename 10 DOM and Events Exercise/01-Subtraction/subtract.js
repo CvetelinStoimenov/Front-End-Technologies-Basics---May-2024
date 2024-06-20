548139bb836969cfc400d204b4cf3919c8569358
CvetelinStoimenov <stoimenovv@gmail.com>
2024-06-20 12:16:15 +0300

    20.06.2024

--- 10 DOM and Events Exercise/01-Subtraction/subtract.js
+++ 10 DOM and Events Exercise/01-Subtraction/subtract.js
@@ -1,3 +1,12 @@
 function subtract() {
-    // TODO...
+    let firstInput = document.getElementById("firstNumber");
+    let secondInput = document.getElementById("secondNumber");
+    let resultDiv = document.getElementById("result");
+
+    let firstValue = Number(firstInput.value);
+    let secondValue = Number(secondInput.value);
+
+    let result = firstValue - secondValue;
+
+    resultDiv.textContent = result;
 } 
--- 10 DOM and Events Exercise/04-Search in List/search.js
+++ 10 DOM and Events Exercise/04-Search in List/search.js
@@ -1,3 +1,19 @@
 function search() {
-   // TODO...
+   let searchedValue = document.getElementById("searchText").value;
+   let liElements = document.querySelectorAll("li");
+   let resultDiv = document.getElementById("result");
+
+   let matches = 0;
+
+   for (let i = 0; i < liElements.length; i++) {
+      if ((liElements[i].textContent).toLowerCase().includes(searchedValue.toLowerCase())) {
+         liElements[i].style.fontWeight = 'bold';
+         liElements[i].style.textDecoration = 'underline';
+         matches++;
+      } else {
+         liElements[i].style.fontWeight = '';
+         liElements[i].style.textDecoration = '';
+      }
+      
+   }
 }
